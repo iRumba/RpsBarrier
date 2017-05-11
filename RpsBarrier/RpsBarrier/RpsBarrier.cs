94d7066fdc2bc756668ea548f4fb9aa783b11eca
iRumba <reloader2003@gmail.com>
2017-05-11 22:09:45 +0700

    Использовал массив вместо динамической коллекции.

--- RpsBarrier/RpsBarrier/RpsBarrier.cs
+++ RpsBarrier/RpsBarrier/RpsBarrier.cs
@@ -5,7 +5,12 @@ namespace RpsBarrier
 {
     public class RpsBarrier
     {
+        DateTime[] _operations;
+        int _firstIndex;
+        int _lastIndex;
+
         int _maxOperationsPerSecond;
+
         public int MaxOperationsPerSecond
         {
             get
@@ -17,16 +22,14 @@ namespace RpsBarrier
                 if (_maxOperationsPerSecond != value)
                 {
                     _maxOperationsPerSecond = value;
-                    _rpsRequestsTimeStamps.Clear();
+                    Reset();
                 }
             }
         }
         static RpsBarrier _instance;
-        LinkedList<DateTime> _rpsRequestsTimeStamps;
 
         RpsBarrier()
         {
-            _rpsRequestsTimeStamps = new LinkedList<DateTime>();
             MaxOperationsPerSecond = 500;
         }
 
@@ -40,27 +43,48 @@ namespace RpsBarrier
 
         public void Reset()
         {
-            _rpsRequestsTimeStamps.Clear();
+            _operations = new DateTime[MaxOperationsPerSecond];
+            _firstIndex = -1;
+            _lastIndex = -1;
         }
 
         public bool CanExecute()
         {
-            // Чистим список таймстампов операций
             ResetTimeStamps();
-
-            // Если текущее количество операций меньше, чем максимально допустимое, 
-            // то разрешаем выполнение и добавляем текущий отпечаток времени в хэшсет
-            var res = _rpsRequestsTimeStamps.Count < MaxOperationsPerSecond;
-            if (res)
-                _rpsRequestsTimeStamps.AddLast(DateTime.UtcNow);
-            return res;
+            return AddLast(DateTime.UtcNow);
         }
 
         void ResetTimeStamps()
         {
-            var first = _rpsRequestsTimeStamps.First;
-            if (first != null && first.Value.AddSeconds(1) < DateTime.UtcNow)
-                _rpsRequestsTimeStamps.RemoveFirst();
+            if (_firstIndex != -1 && _operations[_firstIndex].AddSeconds(1) < DateTime.UtcNow)
+                RemoveFirst();
+        }
+
+        int GetNextIndex(int index)
+        {
+            return index >= MaxOperationsPerSecond - 1 ? 0 : ++index;
+        }
+
+        int GetPrevIndex(int index)
+        {
+            return index <= 0 ? MaxOperationsPerSecond - 1 : --index;
+        }
+
+        bool AddLast(DateTime date)
+        {
+            var next = GetNextIndex(_lastIndex);
+            if (next == _firstIndex)
+                return false;
+            if (_firstIndex == -1)
+                _firstIndex = next;
+            _operations[(_lastIndex = next)] = date;
+            return true;
+        }
+
+        void RemoveFirst()
+        {
+            if (_firstIndex != -1 && _firstIndex != _lastIndex)
+                _firstIndex = GetNextIndex(_firstIndex);
         }
     }
 
